ce9fd44810f9e017a6a46434572a553623bbc1d0
Gandjurov <Gandjurov@abv.bg>
2018-11-10 20:45:41 +0200

    Add Point class

---
+++ 06.Workshop-Snake/ConsoleSnakeV1/SimpleSnake/GameObjects/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Point
+    {
+        private int leftX;
+        private int topY;
+
+        public Point(int leftX, int topY)
+        {
+            this.LeftX = leftX;
+            this.TopY = topY;
+        }
+
+        public int LeftX
+        {
+            get
+            {
+                return leftX;
+            }
+            private set
+            {
+                //TODO think about directions
+                if (value < 0 || value > Console.WindowWidth)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                leftX = value;
+            }
+        }
+        
+        public int TopY
+        {
+            get
+            {
+                return topY;
+            }
+            private set
+            {
+                if (value < 0 || value > Console.WindowHeight)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                topY = value;
+            }
+        }
+
+    }
+}
