b739a30e9e3e4bed94d04d3a2133158dbfd3278f
Gandjurov <gandjurov@abv.bg>
2018-11-10 23:34:14 +0200

    Add Wall.cs

--- 06.Workshop-Snake/ConsoleSnakeV1/SimpleSnake/StartUp.cs
+++ 06.Workshop-Snake/ConsoleSnakeV1/SimpleSnake/StartUp.cs
@@ -1,11 +1,16 @@
 ﻿namespace SimpleSnake
 {
+    using SimpleSnake.GameObjects;
     using Utilities;
+    using System;
 
     public class StartUp
     {
         public static void Main()
         {
+            Wall wall = new Wall(60, 20);
+            Console.ReadLine();
+
             ConsoleWindow.CustomizeConsole();
         }
     }
