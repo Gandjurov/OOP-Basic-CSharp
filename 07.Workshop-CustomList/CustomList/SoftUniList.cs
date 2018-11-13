c0106c81e59821a2fd921ba5f63684e1fbb685f8
Gandjurov <gandjurov@abv.bg>
2018-11-13 19:24:35 +0200

    Add test

--- 07.Workshop-CustomList/CustomList/SoftUniList.cs
+++ 07.Workshop-CustomList/CustomList/SoftUniList.cs
@@ -6,6 +6,25 @@ namespace CustomList
 {
     public class SoftUniList
     {
+        private const int initialCapacity = 2;
+        //add
+        //remove
+        //read
 
+        private string[] array;
+
+        public SoftUniList()
+        {
+            this.array = new string[initialCapacity];
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(string item)
+        {
+            this.array[this.Count] = item;
+            this.Count++;
+        }
     }
 }
--- 07.Workshop-CustomList/CustomList/StartUp.cs
+++ 07.Workshop-CustomList/CustomList/StartUp.cs
@@ -6,7 +6,11 @@ namespace CustomList
     {
         public static void Main(string[] args)
         {
+            SoftUniList softUniList = new SoftUniList();
 
+            softUniList.Add("Gosho");
+            softUniList.Add("Pesho");
+            softUniList.Add("Ivan");
         }
     }
 }
