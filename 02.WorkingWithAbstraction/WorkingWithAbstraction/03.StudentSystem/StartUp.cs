d42e52c12cb50e5daafcafc6bc1fde30f4ed669e
Gandjurov <gandjurov@abv.bg>
2018-11-14 09:51:37 +0200

    Add one more exercise

---
+++ 02.WorkingWithAbstraction/WorkingWithAbstraction/03.StudentSystem/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StudentSystem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StudentSystem system = new StudentSystem();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                system.ParseCommand(command);
+            }
+        }
+    }
+}
---
+++ 02.WorkingWithAbstraction/WorkingWithAbstraction/03.StudentSystem/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentSystem
+{
+    public class Student
+    {
+        private string name;
+        private int age;
+        private double grade;
+
+        public Student(string name, int age, double grade)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Grade = grade;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public double Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+
+        public override string ToString()
+        {
+            string gradeComment = GetGradeComment();
+
+            return $"{Name} is {Age} years old. {gradeComment}";
+        }
+
+        private string GetGradeComment()
+        {
+            if (Grade >= 5)
+            {
+                return "Excellent student.";
+            }
+            else if (Grade >= 3.50)
+            {
+                return "Average student.";
+            }
+            else
+            {
+                return "Very nice person.";
+            }
+        }
+    }
+}
