84430d305dd3bc2dcb4a695b13fd8db2dda66335
Gandjurov <gandjurov@abv.bg>
2018-11-15 20:33:42 +0200

    Add both exercises from refactoring

---
+++ 02.WorkingWithAbstraction/Refactoring/01.RawData/Car.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace RawData
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private Cargo cargo;
+        private string cargoType;
+        private List<Tire> tires;
+
+        public Car(string model, Engine engine, Cargo cargo, List<Tire> tiers)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.Tires = tires;
+        }
+
+        public Engine Engine
+        {
+            get => engine;
+            set => engine = value;
+        }
+
+        public Cargo Cargo
+        {
+            get => cargo;
+            set => cargo = value;
+        }
+
+        public List<Tire> Tires
+        {
+            get => tires;
+            set => tires = value;
+        }
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+    }
+}
+
