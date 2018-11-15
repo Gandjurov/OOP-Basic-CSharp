881385206d69328dfde367c9926c60343456518f
Gandjurov <gandjurov@abv.bg>
2018-11-15 23:18:08 +0200

    more exercises

---
+++ 02.WorkingWithAbstraction/Refactoring/02.CarsSalesman/Car.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace CarsSalesman
+{
+    public class Car
+    {
+        private const string offset = "  ";
+
+        private string model;
+        private Engine engine;
+        private int weight;
+        private string color;
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = -1;
+            this.Color = "n/a";
+        }
+
+        public Car(string model, Engine engine, int weight) : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color) : this(model, engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color) : this(model, engine)
+        {
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public Engine Engine
+        {
+            get => engine;
+            set => engine = value;
+        }
+
+        public int Weight
+        {
+            get => weight;
+            set => weight = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"{this.Model}:\n");
+            sb.Append(this.Engine.ToString());
+
+            var weight = this.Weight == -1 ? "n/a" : this.Weight.ToString();
+            sb.AppendFormat($"{offset}Weight: {weight}\n");
+            sb.AppendFormat($"{offset}Color: {this.Color}");
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ 02.WorkingWithAbstraction/Refactoring/02.CarsSalesman/Engine.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace CarsSalesman
+{
+    public class Engine
+    {
+        private const string offset = "  ";
+
+        private string model;
+        private int power;
+        private int displacement;
+        private string efficiency;
+
+        public Engine(string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+            this.Displacement = -1;
+            this.Efficiency = "n/a";
+        }
+
+        public Engine(string model, int power, int displacement) : this(model, power)
+        {
+            this.Displacement = displacement;
+        }
+
+        public Engine(string model, int power, string efficiency) : this(model, power)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int displacement, string efficiency) : this(model, power)
+        {
+            this.Efficiency = efficiency;
+            this.Displacement = displacement;
+        }
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public int Power
+        {
+            get => power;
+            set => power = value;
+        }
+
+        public int Displacement
+        {
+            get => displacement;
+            set => displacement = value;
+        }
+
+        public string Efficiency
+        {
+            get => efficiency;
+            set => efficiency = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"{offset}{this.Model}:\n");
+            sb.AppendFormat($"{offset}Power: {this.Power}\n");
+
+            var displacement = this.Displacement == -1 ? "n/a" : this.Displacement.ToString();
+            sb.AppendFormat($"{offset}Displacement: {displacement}\n");
+
+            sb.AppendFormat($"{offset}Efficiency: {this.Efficiency}\n");
+
+            return sb.ToString();
+        }
+    }
+
+}
