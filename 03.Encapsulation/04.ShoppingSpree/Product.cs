a0528d1512fb20331ca950862cc472fa0fdaba29
Gandjurov <gandjurov@abv.bg>
2018-11-18 19:51:38 +0200

    one more exercise

---
+++ 03.Encapsulation/04.ShoppingSpree/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    Exception ex = new ArgumentException("Name cannot be empty");
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+                name = value;
+            }
+        }
+        
+        public decimal Cost
+        {
+            get { return cost; }
+            set
+            {
+                if (value < 0)
+                {
+                    Exception ex = new ArgumentException("Money cannot be negative");
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+                cost = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+
+    }
+}
