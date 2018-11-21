512bf0ce828d203c1e312e9de8209045b10fe993
Gandjurov <gandjurov@abv.bg>
2018-11-21 14:58:41 +0200

    Add RandomList

---
+++ 04.Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+        
+        public RandomList()
+        {
+            this.Random = new Random();
+        }
+
+        public Random Random
+        {
+            get { return rnd; }
+            set { rnd = value; }
+        }
+
+        public string RemoveRandomElement()
+        {
+            if (this.Count < 1)
+            {
+                return "No elements available";
+            }
+
+            int index = rnd.Next(0, this.Count - 1);
+            string str = this[index];
+            this.RemoveAt(index);
+
+            return str;
+        }
+
+    }
+}
---
+++ 04.Inheritance/04.RandomList/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("First element");
+            list.Add("Second element");
+            list.Add("Third element");
+            list.Add("Fourth element");
+            list.Add("Fifth element");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(list.RemoveRandomElement());
+            }
+        }
+    }
+}
