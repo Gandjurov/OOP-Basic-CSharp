fbf30effce2c48b6e0bb9a3fe99bedbfffbf1a31
Gandjurov <gandjurov@abv.bg>
2018-11-21 20:00:22 +0200

    add one more exercise

---
+++ 04.Inheritance/05.MordorsCruelPlan/Factories/MoodFactory.cs
@@ -0,0 +1,30 @@
+﻿using MordorsCruelPlan.Moods;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MordorsCruelPlan.Factories
+{
+    public class MoodFactory
+    {
+        public Mood CreateMood(string type)
+        {
+            type = type.ToLower();
+
+            switch (type)
+            {
+                case "angry":
+                    return new Angry();
+                case "happy":
+                    return new Happy();
+                case "sad":
+                    return new Sad();
+                case "javascript":
+                    return new JavaScript();
+                    
+                default:
+                    throw new Exception("Invalid type!");
+            }
+        }
+    }
+}
---
+++ 04.Inheritance/05.MordorsCruelPlan/Foods/Lembas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MordorsCruelPlan.Foods
+{
+    public class Lembas : Food
+    {
+        private const int happiness = 3;
+
+        public Lembas() 
+            : base(happiness)
+        {
+
+        }
+    }
+}
