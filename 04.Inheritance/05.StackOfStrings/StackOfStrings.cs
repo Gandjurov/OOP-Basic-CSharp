6bac8848e03d0c7d4e2a3da901d09e6f17fe1f70
Gandjurov <gandjurov@abv.bg>
2018-11-22 15:48:39 +0200

    Add StackOfStrings

---
+++ 04.Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : List<string>
+    {
+        private List<string> data;
+
+        public List<string> Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public void Push(string element)
+        {
+            Add(element);
+        }
+
+        public string Pop()
+        {
+            string element = GetLastElement();
+            RemoveAt(Count - 1);
+            return element;
+        }
+
+        public string Peek()
+        {
+            return GetLastElement();
+        }
+
+        public bool IsEmpty()
+        {
+            return Count < 1;
+        }
+
+        private string GetLastElement()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("The Stack is empty.");
+            }
+
+            return this.Last();
+        }
+    }
+}
---
+++ 04.Inheritance/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new StackOfStrings();
+
+            stack.Push("Something1");
+            stack.Push("Something2");
+            stack.Push("Something3");
+            stack.Push("Something4");
+            stack.Push("Something5");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+
+        }
+    }
+}
