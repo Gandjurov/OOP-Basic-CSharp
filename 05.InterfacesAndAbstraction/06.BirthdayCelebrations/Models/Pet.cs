2dd5114514e0422011ae6ffe4c3a4045d3fbf96d
Gandjurov <gandjurov@abv.bg>
2018-11-23 16:39:10 +0200

    Add BirthdayCelebrations

---
+++ 05.InterfacesAndAbstraction/06.BirthdayCelebrations/Models/Pet.cs
@@ -0,0 +1,33 @@
+﻿using BirthdayCelebrations.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations.Models
+{
+    public class Pet : IBirthable
+    {
+        private string name;
+        private string birthdate;
+
+        public Pet(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        
+        public string Birthdate
+        {
+            get { return birthdate; }
+            private set { birthdate = value; }
+        }
+
+
+    }
+}
