948cd50ad8144ef705ae63af40104ccb2a8e3335
Gandjurov <gandjurov@abv.bg>
2018-11-23 10:44:13 +0200

    add MultipleImplement exercise

---
+++ 05.InterfacesAndAbstraction/02.MultipleImplement/Citizen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+            
+        public string Birthdate
+        {
+            get { return birthdate; }
+            private set { birthdate = value; }
+        }
+
+
+        public string Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+    }
+}
