40805db5eef4e2d4b2162d67cac562519cee50bb
Gandjurov <gandjurov@abv.bg>
2018-11-23 11:54:31 +0200

    Add Ferrari

---
+++ 05.InterfacesAndAbstraction/03.Ferrari/Ferrari.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferrari
+{
+    public class Ferrari : IDriveable
+    {
+        private string driver;
+        private string model;
+
+        public Ferrari(string driver)
+        {
+            this.Driver = driver;
+            this.Model = "488-Spider";
+        }
+
+        public Ferrari()
+        {
+        }
+
+        public string Driver
+        {
+            get { return driver; }
+            set { driver = value; }
+        }
+        
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        
+        public string PushGas()
+        {
+            return "Zadu6avam sA!";
+        }
+
+        public string UseBrakes()
+        {
+            return "Brakes!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Model}/{UseBrakes()}/{PushGas()}/{Driver}";
+        }
+    }
+}
---
+++ 05.InterfacesAndAbstraction/03.Ferrari/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ferrari
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string driver = Console.ReadLine();
+
+            Ferrari ferrari = new Ferrari(driver);
+            Console.WriteLine(ferrari);
+        }
+    }
+}
