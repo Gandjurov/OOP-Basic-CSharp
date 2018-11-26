b08535f3a90547977d9559a08e8350e6a368e24d
Gandjurov <gandjurov@abv.bg>
2018-11-26 14:17:36 +0200

    add 10.ExplicitInterfaces

---
+++ 05.InterfacesAndAbstraction/10.ExplicitInterfaces/Core/Engine.cs
@@ -0,0 +1,29 @@
+﻿using ExplicitInterfaces.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces.Core
+{
+    public class Engine
+    {
+        public void Run()
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputArgs = input.Split();
+
+                string name = inputArgs[0];
+                string country = inputArgs[1];
+                int age = int.Parse(inputArgs[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                Console.WriteLine(citizen.GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
+        }
+    }
+}
