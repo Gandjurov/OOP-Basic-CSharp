c4bd8caf3d48403147995b1391e83142a43692de
Gandjurov <gandjurov@abv.bg>
2018-11-26 08:54:46 +0200

    Add 08.MilitaryElite

---
+++ 05.InterfacesAndAbstraction/08.MilitaryElite/Contracts/ISoldier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Contracts
+{
+    public interface ISoldier
+    {
+        //id, firstName and lastName
+        int Id { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
---
+++ 05.InterfacesAndAbstraction/08.MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,45 @@
+﻿using MilitaryElite.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Soldier : ISoldier
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+
+        public Soldier(int id, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+        //TODO add validation
+        public int Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+        
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+        
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.FirstName} {this.LastName} Id: {this.Id} ";
+
+        }
+    }
+}
