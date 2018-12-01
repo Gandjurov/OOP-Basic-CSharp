75a0c8244b6f3c68351c6063eb504851adf2d483
Gandjurov <gandjurov@abv.bg>
2018-12-01 14:41:53 +0200

    Add StorageMaster-Contest992

---
+++ 10.ExamPreparationOOP/StorageMaster-Contest992/Models/Products/Gpu.cs
@@ -0,0 +1,17 @@
+﻿using StorageMaster.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageMaster.Models.Products
+{
+    public class Gpu : Product
+    {
+        private const double GpuWeight = 0.7;
+
+        public Gpu(double price) 
+            : base(price, GpuWeight)
+        {
+        }
+    }
+}
---
+++ 10.ExamPreparationOOP/StorageMaster-Contest992/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace StorageMaster
+{
+    using StorageMaster.Core;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StorageMaster storageMaster = new StorageMaster();
+            Engine engine = new Engine(storageMaster);
+            engine.Run();
+        }
+    }
+}
