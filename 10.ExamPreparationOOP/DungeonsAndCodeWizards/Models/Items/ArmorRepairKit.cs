d56078c296acec42e120ebd1c91fbffe1cce67e8
Gandjurov <gandjurov@abv.bg>
2018-12-01 11:05:08 +0200

    Add Dungeons And Code Wizards from Exam preparation

---
+++ 10.ExamPreparationOOP/DungeonsAndCodeWizards/Models/Items/ArmorRepairKit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards.Models.Items
+{
+    public class ArmorRepairKit : Item
+    {
+        private const int initialWeight = 10;
+
+        public ArmorRepairKit() 
+            : base(initialWeight)
+        {
+        }
+
+        public override void AffectCharacter(Character character)
+        {
+            EnsureIsAlive(character);
+
+            character.Armor = character.BaseArmor;
+
+
+        }
+    }
+}
