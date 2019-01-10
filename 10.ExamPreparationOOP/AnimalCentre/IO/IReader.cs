721688aef4a8fa726f96fecedcb25d53f225b546
Gandjurov <gandjurov@abv.bg>
2019-01-10 23:16:59 +0200

    Add Reader and Writer

---
+++ 10.ExamPreparationOOP/AnimalCentre/IO/IReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalCentre.IO
+{
+    public interface IReader
+    {
+        string readData();
+    }
+}
---
+++ 10.ExamPreparationOOP/AnimalCentre/IO/Reader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalCentre.IO
+{
+    public class Reader : IReader
+    {
+        public string readData()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
