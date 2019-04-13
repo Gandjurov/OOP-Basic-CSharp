3e64cf6d18c77c377271f13a0b1886e4ae7d8e78
Gandjurov <gandjurov@abv.bg>
2019-04-13 14:51:27 +0300

    implement RegisterAnimal

---
+++ 11.ExamPrepOOP-TechModule4.0/AnimalCentre/AnimalCentre/Core/AnimalCentre.cs
@@ -0,0 +1,71 @@
+﻿using AnimalCentre.Models.Contracts;
+using AnimalCentre.Core.AnimalFactory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AnimalCentre.Models;
+
+namespace AnimalCentre.Core
+{
+    public class AnimalCentre
+    {
+        private IAnimalFactory animalFactory;
+        private readonly IHotel hotel;
+
+        public AnimalCentre()
+        {
+            this.animalFactory = new AnimalFactory.AnimalFactory();
+            this.hotel = new Hotel();
+        }
+
+        public string RegisterAnimal(string type, string name, int energy, int happiness, int procedureTime)
+        {
+            var animal = this.animalFactory.CreateAnimal(type, name, energy, happiness, procedureTime);
+
+            this.hotel.Accommodate(animal);
+
+            return $"Animal {animal.Name} registered successfully";
+        }
+
+        public string Chip(string name, int procedureTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Vaccinate(string name, int procedureTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Fitness(string name, int procedureTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Play(string name, int procedureTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DentalCare(string name, int procedureTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NailTrim(string name, int procedureTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Adopt(string animalName, string owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string History(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
