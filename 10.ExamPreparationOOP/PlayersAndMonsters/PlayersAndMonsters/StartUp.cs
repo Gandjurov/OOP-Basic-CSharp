d265b4737ad115b268b8ef71a4681406108467a0
Gandjurov <gandjurov@abv.bg>
2019-05-13 21:43:44 +0300

    add StartUp.cs

---
+++ 10.ExamPreparationOOP/PlayersAndMonsters/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace PlayersAndMonsters
+{
+    using Core;
+    using Core.Contracts;
+    using Core.Factories;
+    using Core.Factories.Contracts;
+    using Repositories;
+    using Repositories.Contracts;
+    using Models.BattleFields;
+    using Models.BattleFields.Contracts;
+    using PlayersAndMonsters.IO.Contracts;
+    using PlayersAndMonsters.IO;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
